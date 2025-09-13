db73224d246cdfdfe493e40dc99e2f02a267a632
pranaysriram <162683217+pranaysriram@users.noreply.github.com>
2025-09-13 19:01:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+<<<<<<< HEAD
+async function loadProducts() {
+  const res = await fetch("http://localhost:5000/api/products");
+  const products = await res.json();
+
+  const container = document.getElementById("products");
+  container.innerHTML = "";
+
+  products.forEach(p => {
+    container.innerHTML += `
+      <div class="card">
+        <img src="${p.image}" alt="${p.name}">
+        <h3>${p.name}</h3>
+        <p>₹${p.price}</p>
+        <p>${p.available ? "✅ Available" : "❌ Out of Stock"}</p>
+      </div>
+    `;
+  });
+}
+
+loadProducts();
+=======
+async function loadProducts() {
+  const res = await fetch("http://localhost:5000/api/products");
+  const products = await res.json();
+
+  const container = document.getElementById("products");
+  container.innerHTML = "";
+
+  products.forEach(p => {
+    container.innerHTML += `
+      <div class="card">
+        <img src="${p.image}" alt="${p.name}">
+        <h3>${p.name}</h3>
+        <p>₹${p.price}</p>
+        <p>${p.available ? "✅ Available" : "❌ Out of Stock"}</p>
+      </div>
+    `;
+  });
+}
+
+loadProducts();
+>>>>>>> d58989b79c0319419750a96b365575c714a9af4d
